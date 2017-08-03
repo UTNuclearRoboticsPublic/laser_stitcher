2553e7eab7725057a438d1a6b4469d56f886f5af
AndyZe <andyz@utexas.edu>
2017-08-03 16:54:21 -0500

    Added node to control UR5 joints through URScript to allow wrist3 to be rotated (rotating the LIDAR), continued tweaking laser_stitcher

--- include/laser_stitcher/laser_stitcher.h
+++ include/laser_stitcher/laser_stitcher.h
@@ -12,6 +12,10 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <rosbag/bag.h>
 
+//#include <tf2_ros/transform_listener.h>
+
+//#include <tf2/buffer_core.h>
+
 class LaserStitcher
 {
 public: 
@@ -24,15 +28,27 @@ private:
 
 	bool is_running_;
 	bool save_data_;
+	bool publish_after_updating_;
 	std::string bag_name_;
 	std::string target_frame_;
 	sensor_msgs::PointCloud2 summed_pointcloud_;
+	float sleepy_time_;
+	int publishing_throttle_;
+	int throttle_index_;
+
+	bool should_check_movement_;
+	float distance_threshold_;
+	float angle_threshold_;
 
 	laser_geometry::LaserProjection scan_converter_;
 	tf::TransformListener listener_;
+	//tf2_ros::Buffer tfBuffer_;
+	//tf2::BufferCore buffer_core_;
+	tf::StampedTransform last_transform_;
 
 	void laserCallback(const sensor_msgs::LaserScan::ConstPtr& scan_in);
 	void setScanningState(const std_msgs::Bool::ConstPtr& shut_down);
+	bool lidarHasMoved(std::string);
 };
 
 #endif //LASER_STITCHER_H
