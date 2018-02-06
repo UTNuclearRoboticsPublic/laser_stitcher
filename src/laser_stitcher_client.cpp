a1039e957bb55da16d75cd7e503837eab9ac6161
AndyZe <andyz@utexas.edu>
2018-02-06 15:15:47 -0600

    Adding launch/config files that wait for a service req before scanning. Removing old 'handle_turning' demo. Not conducting a scan on startup, by default, unless loop is True (safety issue).

--- src/laser_stitcher_client.cpp
+++ src/laser_stitcher_client.cpp
@@ -14,33 +14,32 @@ int main(int argc, char** argv)
 	bool should_loop;
 	nh.param<bool>("laser_stitcher/should_loop", should_loop, false);
 
-	while(ros::ok())
+	if (should_loop)
 	{
-		laser_stitcher::stationary_scan scan_srv;
-		float temp_angle;
-		nh.param<float>("lidar_ur5_manager/min_angle", temp_angle, -1.57);
-		scan_srv.request.min_angle = temp_angle;
-		nh.param<float>("lidar_ur5_manager/max_angle", temp_angle, 1.57);
-		scan_srv.request.max_angle = temp_angle;
-		scan_srv.request.external_angle_sensing = false;
+		while(ros::ok())
+		{
+			laser_stitcher::stationary_scan scan_srv;
+			float temp_angle;
+			nh.param<float>("lidar_ur5_manager/min_angle", temp_angle, -1.57);
+			scan_srv.request.min_angle = temp_angle;
+			nh.param<float>("lidar_ur5_manager/max_angle", temp_angle, 1.57);
+			scan_srv.request.max_angle = temp_angle;
+			scan_srv.request.external_angle_sensing = false;
 
-		ros::Duration(2.0).sleep();
-	
-		if( ! scanning_client.call(scan_srv) )
-			ROS_WARN_STREAM("[LaserStitcherClient] Scanning service call failed - prob not up yet");
-		else
-		{	
-			ROS_INFO_STREAM("[LaserStitcherClient] Successfully called scanning service.");
-			for(int i=0; i<scan_srv.response.output_clouds.size(); i++)
-			{
-				ROS_INFO_STREAM("\t " << scan_srv.response.cloud_names[i] << " cloud size: " << scan_srv.response.output_clouds[i].height*scan_srv.response.output_clouds[i].width << " in frame " << scan_srv.response.output_clouds[i].header.frame_id);
+			ros::Duration(2.0).sleep();
+		
+			if( ! scanning_client.call(scan_srv) )
+				ROS_WARN_STREAM("[LaserStitcherClient] Scanning service call failed - prob not up yet");
+			else
+			{	
+				ROS_INFO_STREAM("[LaserStitcherClient] Successfully called scanning service.");
+				for(int i=0; i<scan_srv.response.output_clouds.size(); i++)
+				{
+					ROS_INFO_STREAM("\t " << scan_srv.response.cloud_names[i] << " cloud size: " << scan_srv.response.output_clouds[i].height*scan_srv.response.output_clouds[i].width << " in frame " << scan_srv.response.output_clouds[i].header.frame_id);
+				}
 			}
 		}
-
-		// If we shouldn't loop, break the loop
-		if(!should_loop)
-			break;
-	}	
+	}
 	
 	ros::Duration(1.0).sleep();
 
